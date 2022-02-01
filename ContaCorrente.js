de98e455f2105c463a50c2e58de174685036ae8e
XxDiogoX <diogorodriguesk02@gmail.com>
2022-02-01 03:46:34 -0300

    Metodo depositar criado, e o atributo _saldo como atributo privado, acesso somente com o metodo modificador set

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,14 +3,29 @@ export class ContaCorrente{
     numeroConta;
     cliente;
 
-    _saldo;
+    #_saldo // Atributo privado
+
+    set saldo(valorSaldo){
+        this.#_saldo = valorSaldo;
+    }
+
 
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            console.log(`Valor Sacado: ${valor.toFixed(2)}\nSaldo: ${this._saldo.toFixed(2)}`);
+        if(this.#_saldo >= valor){
+            this.#_saldo -= valor;
+            console.log(`Valor Sacado: ${valor.toFixed(2)}\nSaldo: ${this.#_saldo.toFixed(2)}`);
+            return valor;
         }else{
-            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}\nSaldo: ${this._saldo.toFixed(2)}`);
+            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}\nSaldo: ${this.#_saldo.toFixed(2)}`);
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.#_saldo += valor;
+            console.log(`Depósito: ${valor.toFixed(2)}`);
+        }else{
+            console.log(`Depósito R$ ${valor} Inválido!!`);
         }
     }
 
--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const conta1 = new ContaCorrente();
-const cli1 = new Cliente();
-cli1.nome = "Diogo Rodrigues";
-
-conta1._saldo = 500;
-conta1.agencia = 3574;
-conta1.numeroConta = 96452;
-conta1.cliente = cli1.nome;
-
-console.log(conta1);
-
-conta1.sacar(200);
