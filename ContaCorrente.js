da3bbba516f7cb9ad39a744a5875987d8cc73a7d
XxDiogoX <diogorodriguesk02@gmail.com>
2022-02-01 03:07:19 -0300

    Criado o metodo sacar na classe ContaCorrente

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -5,6 +5,13 @@ export class ContaCorrente{
 
     _saldo;
 
-   
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            console.log(`Valor Sacado: ${valor.toFixed(2)}\nSaldo: ${this._saldo.toFixed(2)}`);
+        }else{
+            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}\nSaldo: ${this._saldo.toFixed(2)}`);
+        }
+    }
 
 }
