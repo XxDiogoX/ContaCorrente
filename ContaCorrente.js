7cee7413944a16fabcbf59d63c0fdf1a899f01a7
XxDiogoX <diogorodriguesk02@gmail.com>
2022-02-01 04:47:37 -0300

    Metodo transferir criado na classe ContaCorrente

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -9,24 +9,41 @@ export class ContaCorrente{
         this.#_saldo = valorSaldo;
     }
 
+    mostrarSaldo(){
+        console.log("----------- Saldo -------------");
+        console.log(`Titular: ${this.cliente.nome}\nSaldo: R$ ${this.#_saldo.toFixed(2)}`);
+        console.log("-------------------------------");
+    }
+
 
     sacar(valor){
         if(this.#_saldo >= valor){
             this.#_saldo -= valor;
-            console.log(`Valor Sacado: ${valor.toFixed(2)}\nSaldo: ${this.#_saldo.toFixed(2)}`);
+            console.log(`Valor Sacado: R$ ${valor.toFixed(2)}\nSaldo: R$ ${this.#_saldo.toFixed(2)}`);
             return valor;
         }else{
-            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}\nSaldo: ${this.#_saldo.toFixed(2)}`);
+            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}\nSaldo: R$ ${this.#_saldo.toFixed(2)}`);
         }
     }
 
     depositar(valor){
         if(valor > 0){
             this.#_saldo += valor;
-            console.log(`Depósito: ${valor.toFixed(2)}`);
+            console.log(`Depósito: R$ ${valor.toFixed(2)}`);
         }else{
-            console.log(`Depósito R$ ${valor} Inválido!!`);
+            console.log(`Depósito: R$ ${valor} Inválido!!`);
         }
+    }
+
+    transferir(valor,conta){
+        if(this.#_saldo >= valor && conta instanceof ContaCorrente){
+            const valorSacado = this.sacar(valor);
+            conta.depositar(valorSacado);
+            console.log(`R$ ${valorSacado.toFixed(2)} transferido para ${conta.cliente.nome}`);
+        }else{
+            console.log("Não foi possível transferir!!");
+        }
+
     }
 
 }
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
+
+
+
+
